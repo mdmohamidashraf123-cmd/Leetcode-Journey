7be809119baf518e95b62dd95f312af2cf0398b2
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-07-18 16:46:50 +0530

    Time: 0 ms (100%), Space: 9 MB (18.5%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -5,55 +5,26 @@ public:
         if(z==1){
             return false;
         }
-        int a=0;
-        int b=0;
-        int c=0;
-        int d=0;
-        int e=0;
-        int f=0;
-        stack<int>p;
+        stack<char>p;
         for(int i=0;i<z;i++){
-          if(p.size()!=0){
-            if(s[i]==')'){
-                d++;
-                if(p.top()=='('){
-                    p.pop();
-                    continue;
-                }else{
-                    return false;
-                }
-            }else if(s[i]=='}'){
-                e++;
-                if(p.top()=='{'){
-                    p.pop();
-                    continue;
-                }else{
-                    return false;
-                }
-            }else if(s[i]==']'){
-                f++;
-                if(p.top()=='['){
-                    p.pop();
-                    continue;
-                }else{
-                    return false;
-                }
-            }
+          if(s[i]=='['|| s[i]=='{'|| s[i]=='('){
+            p.push(s[i]);
           }else{
-             if(s[i]==')'|| s[i]=='}' || s[i]==']' ){
+            if(p.empty()){
                 return false;
-             }
+            }else{
+                if(s[i]==']'&& p.top()!='['){
+                    return false;
+                }else if(s[i]=='}' && p.top()!='{'){
+                    return false;
+                }else if(s[i]==')' &&p.top()!='(' ){
+                    return false;
+                }
+                p.pop();
+            }
           }
-          if(s[i]=='('){
-            a++;
-          }else if(s[i]=='{'){
-            b++;
-          }else if(s[i]=='['){
-            c++;
-          }
-           p.push(s[i]);
         }
-        if(a!=d || b!=e || c!=f){
+        if(!p.empty()){
             return false;
         }
         return true;
