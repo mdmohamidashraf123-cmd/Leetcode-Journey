7098c8153e8760ad203a5a751493c821b324cfb2
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-07-21 11:36:47 +0530

    Time: 27 ms (5%), Space: 30.6 MB (6.81%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,6 +10,18 @@
  */
 class Solution {
 public:
+    void ab(ListNode* a,stack<ListNode*>& s){
+        if(!a->next || !a->next->next){
+            return ;
+        }
+        ListNode* nn=a->next;
+        a->next=s.top();
+        s.top()->next=nn;
+        s.pop();
+        s.top()->next=nullptr;
+        a=a->next->next;
+        ab(a,s);
+    }
     void reorderList(ListNode* head) {
         ListNode* temp=head;
         ListNode* a=head;
@@ -18,19 +30,6 @@ public:
             s.push(temp);
             temp=temp->next;
         }
-        int c=0;
-        while(a){
-            if(a->next && a->next->next){
-               ListNode* nn=a->next;
-               a->next=s.top();
-               s.top()->next=nn;
-               s.pop();
-               s.top()->next=nullptr;
-               a=a->next->next;
-            }else{
-                break;
-            }
-        }
-        
+        ab(a,s);
     }
 };
