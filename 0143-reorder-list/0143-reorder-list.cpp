bff1b1c9dd2e923b523ae5d1f2a459603428183e
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-07-21 15:53:48 +0530

    Time: 0 ms (100%), Space: 23 MB (23.86%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,26 +10,45 @@
  */
 class Solution {
 public:
-    void ab(ListNode* a,stack<ListNode*>& s){
-        if(!a->next || !a->next->next){
-            return ;
-        }
-        ListNode* nn=a->next;
-        a->next=s.top();
-        s.top()->next=nn;
-        s.pop();
-        s.top()->next=nullptr;
-        a=a->next->next;
-        ab(a,s);
-    }
     void reorderList(ListNode* head) {
         ListNode* temp=head;
-        ListNode* a=head;
-        stack<ListNode*>s;
-        while(temp){
-            s.push(temp);
-            temp=temp->next;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        ListNode* prev=nullptr;
+        if(!head || !head->next){
+            return ;
         }
-        ab(a,s);
+        while(fast && fast->next){
+            temp=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        //reverse slow to last node
+        while(slow){
+         ListNode* nn =slow->next;
+         slow->next=prev;
+         prev=slow;
+         if(!nn){
+            break;
+         }
+         slow=nn;
+        }
+        temp->next=nullptr;
+        //now merge
+        temp=head;
+
+        // b=slow;
+        while( temp && prev){
+            ListNode* nn =temp->next;
+            ListNode* z=prev->next;
+            temp->next=prev;
+            if(nn){
+              prev->next=nn;
+            }
+            prev=z;
+            temp=nn;
+        }
+        
     }
 };
