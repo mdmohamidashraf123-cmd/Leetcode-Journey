c5d79a460a9bdd465704f846a14e7a0398609ce3
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-07-25 15:03:14 +0530

    Time: 0 ms (100%), Space: 9.1 MB (81.11%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    string decodeString(string s) {
-        stack<string>p;
-        stack<int>n;
-        int ans=0;
-        string z="";
+    string  q(int& i,string &s){
         string current="";
-        for(char c:s){
-            if(isdigit(c)){
-               ans=ans*10+c-'0';
-            }else if(c=='['){
-                n.push(ans);
-                p.push(current);
-                current="";
-                ans=0;
-            }else if(c==']'){
-                   int r=n.top();
-                   n.pop();
-                   string prev=p.top();
-                   p.pop();
-                   string temp="";
-                  for(int i=0;i<r;i++){
-                    temp+=current;
-                  }
-                  current=prev+temp;
+        while(i<s.size() && s[i]!=']'){
+            if(!isdigit(s[i])){
+                current+=s[i];
+                i++;
             }else{
-               current +=c;
+                int ans=0;
+                while(i<s.size() && isdigit(s[i])){
+                    ans=ans*10+s[i]-'0';
+                    i++;
+                }
+                i++;
+               string nested=q(i,s);
+               i++;
+                while(ans>0){
+                    current+=nested;
+                    ans--;
+                }
             }
         }
-        
         return current;
+        
+    }
+    string decodeString(string s) {
+        int i=0;
+        return q(i,s);
     }
 };
