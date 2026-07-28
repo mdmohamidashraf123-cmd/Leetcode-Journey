5a9f0e72ea0a69d93a252599993ef446b75858c3
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-07-28 23:33:47 +0530

    Time: 0 ms (100%), Space: 14.8 MB (7.69%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -3,13 +3,11 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int>ans;
         //using hashing
-        int f[1000]={0};
+        int f[1001]={0};
         for(int i:nums1){
-            if(f[i]==0){
-                f[i]++;
-            }
+            f[i]++;
         }
-        int a[1000]={0};
+        int a[1001]={0};
         for(int i:nums2){
             a[i]++;
             if(f[i]>0 && a[i]==1 ){
