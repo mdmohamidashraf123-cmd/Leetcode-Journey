b1dee971eb503d0bc2d73092a8a449bb704d4e0c
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-07-29 22:49:04 +0530

    Time: 0 ms (100%), Space: 14.7 MB (49.59%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
         vector<int>ans;
         int hash[1001]={0};
         for(int i:nums1){
             hash[i]++;
         }
-        int i=0;
-        int c=0;
-        while(i<nums2.size()){
-            if(hash[nums2[i]]>0){
-                c++;
-                int a=i+1;
-                while(a<nums2.size() && nums2[a]==nums2[i]){
-                 c++;
-                 a++;
-                }
-               c=min(hash[nums2[i]],c);
-               for(int k=0;k<c;k++){
-                ans.push_back(nums2[i]);
-               }
-               i=a;
-               c=0;
-            }else{
-                i++;
+        for(int i:nums2){
+            if(hash[i]>0){
+             ans.push_back(i);
+             hash[i]--;
             }
         }
         return ans;
