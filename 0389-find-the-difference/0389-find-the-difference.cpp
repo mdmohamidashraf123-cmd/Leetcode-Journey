f0dfe2de9b76ede5e7c2953612e12f483abb7c95
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-07-29 23:18:26 +0530

    Time: 0 ms (100%), Space: 9.2 MB (51.97%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int>m;
-        for(char i:s){
-            m[i]++;
-        }
-        for(char i:t){
-            if(m[i]==0){
-                return i;
-            }
-            m[i]--;
-        }
-        return std::string::npos;
-
+       char res=0;
+       for(char c:s){
+        res^=c;
+       }
+       for(char c:t){
+        res^=c;
+       }
+       return res;
     }
 };
