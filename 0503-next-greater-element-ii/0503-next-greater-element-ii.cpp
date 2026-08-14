3a060f61828b1dceb1006bb6292dccceda6f3b31
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-08-15 00:21:32 +0530

    Time: 4 ms (59.05%), Space: 28.5 MB (40.18%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,45 +3,20 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
         vector<int>ans(n);
-        if(n==1){
-            ans[0]=-1;
-            return ans;
-        }
-        stack<int>b;
-        stack<int>a;
-        bool hash[10000]={true};
-        ans[n-1]=-1;
-        hash[n-1]=false;
-        a.push(nums[n-1]);
-        for(int i=n-2;i>=0;i--){
-            while(!a.empty() && a.top()<=nums[i]){
-                a.pop();
+        stack<int>st;
+        //optimized t.c and s.c
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.empty()&& st.top()<=nums[i%n]){
+                st.pop();
             }
-            if(a.empty()){
-                hash[i]=false;
-            }else{
-                hash[i]=true;
-                ans[i]=a.top();
-            }
-            a.push(nums[i]);
-            b.push(nums[i]);
-        }
-        int k=0;
-        for(int i=n-1;i>=0;i--){
-            if(!hash[i]){
-                while(i!=0 &&!b.empty() && b.top()<=nums[i]){
-                    b.pop();
-                    k++;
-                    if(k==i){
-                        break;
-                    }
-                }
-                if(b.empty() || k==i||i==0){
-                    ans[i]=-1;
+            if(i<n){
+                if(!st.empty()){
+                 ans[i]=st.top();
                 }else{
-                    ans[i]=b.top();
+                    ans[i]=-1;
                 }
             }
+            st.push(nums[i%n]);
             
         }
         return ans;
