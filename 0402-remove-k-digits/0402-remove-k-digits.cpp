637deece9ac6b2f07fb33450080850f138e72d33
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-08-16 16:20:09 +0530

    Time: 0 ms (100%), Space: 10.8 MB (96.35%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -4,39 +4,25 @@ public:
         if(num.size()==k){
             return "0";
         }
-        stack<int>st;
         int n=num.size();
         int x=n-k;
+        string ans;
         for(int i=0;i<num.size();i++){
-            while(!st.empty() && st.top()>(num[i]-'0') && (n-i)>x-st.size()){
-                st.pop();
+            while(!ans.empty() && ans[ans.size()-1]>(num[i]) && k>0){
+                ans.pop_back();
+                k--;
             }
-            if(st.size()<x){
-             st.push(num[i]-'0');
+            if(ans.size()!=0||(num[i]!='0')){
+             ans.push_back(num[i]);
             }
         }
-        if(st.empty()){
+        while (!ans.empty() && k > 0) {
+            ans.pop_back();
+            k--;
+        }
+        if(ans.empty()){
             return "0";
         }
-        string ans;
-        while(!st.empty()){
-            ans+=to_string(st.top());
-            st.pop();
-        }
-        if(ans.size()==1){
-            return ans;
-        }
-        int a=ans.size();
-        int i=a-1;
-        //deal with leading zeroes
-        while(i>=0 && ans[i]=='0'){
-            i--;
-        }
-        if(i==-1){
-           return "0";
-        }
-        ans.resize(i+1);
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
