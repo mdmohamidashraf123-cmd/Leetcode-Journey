fbe98af3d14a792c4e0d9244cd37d34f95470ff0
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-08-22 12:35:31 +0530

    Time: 0 ms (100%), Space: 21.5 MB (97.3%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int>ans;
-        stack<int>st;
+        //doing in place but not recommended in interviews
         int n=asteroids.size();
+        int j=-1;
         for(int i=0;i<n;i++){
-            if(asteroids[i]>0){
-                ans.push_back(asteroids[i]);
-            }else{
-               while(!ans.empty() &&ans.back()>0 &&  ans.back()<(-asteroids[i])){
-                 ans.pop_back();
-               }
-               if(!ans.empty() && ans.back()==(-asteroids[i])){
-                ans.pop_back();
-               }else if(ans.empty()|| ans.back()<0){
-                ans.push_back(asteroids[i]);
-               }
+            int curr=asteroids[i];
+            bool destroyed=false;
+            while( j>=0 && asteroids[j]>0 &&curr<0){
+               if(asteroids[j]<-curr){
+                j--;
+               }else if(asteroids[j]==-curr){
+                j--;
+                destroyed=true;
+                break;
+               }else{
+                destroyed=true;
+                break;
+               } 
+            }
+            if(!destroyed){
+                asteroids[j+1]=curr;
+                j++;
             }
         }
-       return ans;
+       asteroids.resize(j+1);
+       return asteroids;
     }
 };
