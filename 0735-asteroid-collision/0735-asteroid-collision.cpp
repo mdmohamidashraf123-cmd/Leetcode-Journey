9848857f6459617e60e2fd764a9e3d748437f053
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-08-22 11:57:20 +0530

    Time: 0 ms (100%), Space: 21.8 MB (71.91%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int>ans;
+        stack<int>st;
+        int n=asteroids.size();
+        for(int i=0;i<n;i++){
+            if(asteroids[i]>0){
+                ans.push_back(asteroids[i]);
+            }else{
+               while(!ans.empty() &&ans.back()>0 &&  ans.back()<(-asteroids[i])){
+                 ans.pop_back();
+               }
+               if(!ans.empty() && ans.back()==(-asteroids[i])){
+                ans.pop_back();
+               }else if(ans.empty()|| ans.back()<0){
+                ans.push_back(asteroids[i]);
+               }
+            }
+        }
+       return ans;
+    }
+};
