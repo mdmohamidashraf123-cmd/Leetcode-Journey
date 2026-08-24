4b9dc58dae53d4bba732f750a7b5901c19378dea
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-08-24 14:29:36 +0530

    Time: 16 ms (81.04%), Space: 79.5 MB (87.51%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,52 +1,28 @@
 class Solution {
 public:
-    // vector<int>pse(vector<int>& heights){
-    //     int n=heights.size();
-    //     vector<int>ans(n);
-    //     stack<int>st;
-    //     for(int i=0;i<n;i++){
-    //         while(!st.empty() && heights[st.top()]>=heights[i]){
-    //             st.pop();
-    //         }
-    //         ans[i]=st.empty()? -1 : st.top();
-    //         st.push(i);
-    //     }
-    //     return ans;
-    // }
-    // vector<int>nse(vector<int>& heights){
-    //     int n=heights.size();
-    //     vector<int>ans(n);
-    //     stack<int>st;
-    //     for(int i=n-1;i>=0;i--){
-    //         while(!st.empty() && heights[st.top()]>heights[i]){
-    //             st.pop();
-    //         }
-    //         ans[i]=st.empty()? n: st.top();
-    //         st.push(i);
-    //     }
-    //     return ans;
-    // }
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
-        vector<int>ps(n);
         stack<int>st;
-        vector<int>ns(n,n);
         int ans=0;
+        int ps;
+        int ns;
         for(int i=0;i<n;i++){
            while(!st.empty() && heights[st.top()]>=heights[i]){
-            ns[st.top()]=i;
-            int width=ns[st.top()]-ps[st.top()]-1;
-            ans=max(ans,width*heights[st.top()]);
+            int k=st.top();
             st.pop();
+            ps=st.empty() ? -1: st.top();
+            int width=i-ps-1;
+            ans=max(ans,width*heights[k]);
            }
-           ps[i]=st.empty()? -1 : st.top();
            st.push(i);
         }
         while(!st.empty()){
-            ns[st.top()]=n;
-            int width=ns[st.top()]-ps[st.top()]-1;
-            ans=max(ans,width*heights[st.top()]);
+            ns=n;
+            int k=st.top();
             st.pop();
+            ps=st.empty()? -1:st.top();
+            int width=ns-ps-1;
+            ans=max(ans,width*heights[k]);
         }
         return ans;
     }
