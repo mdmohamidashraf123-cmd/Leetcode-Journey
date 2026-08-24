ad560e5237ac78c7ee4fa9235e1c379356688541
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-08-24 14:01:51 +0530

    Time: 15 ms (86.02%), Space: 85.8 MB (24.56%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,41 +1,52 @@
 class Solution {
 public:
-    vector<int>pse(vector<int>& heights){
-        int n=heights.size();
-        vector<int>ans(n);
-        stack<int>st;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
-                st.pop();
-            }
-            ans[i]=st.empty()? -1 : st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-    vector<int>nse(vector<int>& heights){
-        int n=heights.size();
-        vector<int>ans(n);
-        stack<int>st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && heights[st.top()]>heights[i]){
-                st.pop();
-            }
-            ans[i]=st.empty()? n: st.top();
-            st.push(i);
-        }
-        return ans;
-    }
+    // vector<int>pse(vector<int>& heights){
+    //     int n=heights.size();
+    //     vector<int>ans(n);
+    //     stack<int>st;
+    //     for(int i=0;i<n;i++){
+    //         while(!st.empty() && heights[st.top()]>=heights[i]){
+    //             st.pop();
+    //         }
+    //         ans[i]=st.empty()? -1 : st.top();
+    //         st.push(i);
+    //     }
+    //     return ans;
+    // }
+    // vector<int>nse(vector<int>& heights){
+    //     int n=heights.size();
+    //     vector<int>ans(n);
+    //     stack<int>st;
+    //     for(int i=n-1;i>=0;i--){
+    //         while(!st.empty() && heights[st.top()]>heights[i]){
+    //             st.pop();
+    //         }
+    //         ans[i]=st.empty()? n: st.top();
+    //         st.push(i);
+    //     }
+    //     return ans;
+    // }
     int largestRectangleArea(vector<int>& heights) {
         int n=heights.size();
-        vector<int>ps=pse(heights);
-        vector<int>ns=nse(heights);
-        int ans=INT_MIN;
+        vector<int>ps(n);
+        stack<int>st;
+        vector<int>ns(n,n);
+        int ans=0;
         for(int i=0;i<n;i++){
-           int p=ps[i];
-           int n=ns[i];
-           int area=heights[i]*(n-p-1);
-           ans=max(ans,area);
+           while(!st.empty() && heights[st.top()]>=heights[i]){
+            ns[st.top()]=i;
+            int width=ns[st.top()]-ps[st.top()]-1;
+            ans=max(ans,width*heights[st.top()]);
+            st.pop();
+           }
+           ps[i]=st.empty()? -1 : st.top();
+           st.push(i);
+        }
+        while(!st.empty()){
+            ns[st.top()]=n;
+            int width=ns[st.top()]-ps[st.top()]-1;
+            ans=max(ans,width*heights[st.top()]);
+            st.pop();
         }
         return ans;
     }
