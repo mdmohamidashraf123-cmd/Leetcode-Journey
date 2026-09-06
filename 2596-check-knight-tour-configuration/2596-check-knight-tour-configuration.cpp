f05cf2cb0bf0e7e5c15d4d9d2f75554b95a9a5da
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-09-06 21:57:04 +0530

    Time: 0 ms (100%), Space: 31.8 MB (35.11%) - LeetHub

--- 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -1,84 +1,69 @@
 class Solution {
 public:
+    bool valid(vector<vector<int>>& grid,int i,int j){
+        if(grid[i][j]==pow(grid.size(),2)-1){
+           return true;
+        }
+        bool x=false;
+        int n=grid.size();
+        int c=grid[i][j];
+        if(i+2<n && i+2>=0 && !x){
+                if(j>0 && j<n){
+                    if(grid[i+2][j-1]==c+1){
+                        
+                        x=valid(grid,i+2,j-1);
+                    }
+                }
+                if(j<n-1&& j>=0 &&!x){
+                    if(grid[i+2][j+1]==c+1){
+                        x=valid(grid,i+2,j+1);
+                    }
+                }
+        }
+        if(i-2>=0 && i-2<n&& !x ){
+            if(j>0 && j<n){
+                if(grid[i-2][j-1]==c+1){
+                    x= valid(grid,i-2,j-1);
+                }
+            }
+            if(j<n-1&& !x&& j>=0 ){
+                if(grid[i-2][j+1]==c+1){
+                    x=valid(grid,i-2,j+1);
+                }
+            }
+        }
+        if(j-2>=0 && j-2<n&& !x){
+            if(i>0 && i<n){
+               if(grid[i-1][j-2]==c+1){
+                    x=valid(grid,i-1,j-2);
+                }
+            }
+            if(i<n-1 && i>=0 && !x ){
+                if(grid[i+1][j-2]==c+1){
+                    x=valid(grid,i+1,j-2);
+                }
+            }
+        }
+        if(j+2<n && j+2>=0 && !x){
+            if(i>0 && i<n ){
+                if(grid[i-1][j+2]==c+1){
+                    x=valid(grid,i-1,j+2);
+                }
+            }
+            if(i<n-1&& i>=0&&!x ){
+                if(grid[i+1][j+2]==c+1){
+                    x=valid(grid,i+1,j+2);
+                }
+            }
+        }
+        return x;
+    }
     bool checkValidGrid(vector<vector<int>>& grid) {
         if(grid[0][0]!=0){
             return false;
         }
         int n=grid.size();
-        int m=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                m=max(m,grid[i][j]);
-            }
-        }
-        for(int i=0;i<n;i++){
-          for(int j=0;j<n;j++){
-            if(grid[i][j]==m) {
-                continue;
-            }
-             int c=grid[i][j];
-             bool x=false;
-             if(i+2<n){
-                if(j>0){
-                    if(grid[i+2][j-1]==c+1){
-                        x=true;
-                        continue;
-                    }
-                }
-                if(j<n-1){
-                    if(grid[i+2][j+1]==c+1){
-                        x=true;
-                        continue;
-                    }
-                }
-            }
-            if(i-2>=0){
-                if(j>0){
-                    if(grid[i-2][j-1]==c+1){
-                        x=true;
-                        continue;
-                    }
-                }
-                if(j<n-1){
-                    if(grid[i-2][j+1]==c+1){
-                        x=true;
-                        continue;
-                    }
-                }
-            }
-            if(j-2>=0){
-               if(i>0){
-                    if(grid[i-1][j-2]==c+1){
-                        x=true;
-                        continue;
-                    }
-                }
-                if(i<n-1){
-                    if(grid[i+1][j-2]==c+1){
-                        x=true;
-                        continue;
-                    }
-                }
-            }
-            if(j+2<n){
-               if(i>0){
-                    if(grid[i-1][j+2]==c+1){
-                        x=true;
-                        continue;
-                    }
-                }
-                if(i<n-1){
-                    if(grid[i+1][j+2]==c+1){
-                        x=true;
-                        continue;
-                    }
-                }
-            }
-            if(x==false){
-                return false;
-            }
-          }
-        }
-        return true;
+        return valid(grid,0,0);
+        
     }
 };
