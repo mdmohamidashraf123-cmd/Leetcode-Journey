789b87784833734f9f9628e6427bad5bd66f6780
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-09-12 13:54:02 +0530

    Time: 0 ms (100%), Space: 8.2 MB (81.97%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
+        //Better readability
         int i=0;
         int j=0;
-        long long a=0;
-        long long b=0;
-        while(i<version1.size() && j<version2.size()){
+        while(i<version1.size() || j<version2.size()){
+            long long num1=0;
+            long long num2=0;
             while(i<version1.size() && version1[i]!='.'){
-                a=a*10+version1[i]-'0';
+                num1=num1*10+version1[i]-'0';
                 i++;
             }
             while(j<version2.size() && version2[j]!='.'){
-                b=b*10+version2[j]-'0';
+                num2=num2*10+version2[j]-'0';
                 j++;
             }
-            if(a<b){
+            if(num1<num2){
                 return -1;
-            }else if(a>b){
+            }else if(num1>num2){
                 return 1;
             }
-            a=0;
-            b=0;
             i++;
             j++;
-        }
-        
-        if(i<version1.size()){
-         while(i<version1.size()){
-            
-            a=version1[i]!='.'?a*10+version1[i]-'0':a;
-            i++;
-         }
-         return a>0?1:0;
-        }else if(j<version2.size()){
-         while(j<version2.size()){
-            b=version2[j]!='.'?b*10+version2[j]-'0':b;
-            j++;
-         }
-         return b>0?-1:0;
         }
         return 0;
     }
