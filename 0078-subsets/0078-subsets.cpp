b3e2584320e5f392c3241a37dc1386602e1b3488
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-09-14 15:03:55 +0530

    Time: 0 ms (100%), Space: 10 MB (59.98%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void subas(vector<int>& nums,vector<int>&subs,int n,vector<vector<int>>&ans){
-        if(n==nums.size()){
-            ans.push_back(subs);
-            return;
-        }
-        //include
-        subs.push_back(nums[n]);
-        subas(nums,subs,n+1,ans);
-        subs.pop_back();
-        //exclude
-        subas(nums,subs,n+1,ans);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>subs;
         vector<vector<int>>ans;
-        int n=0;
-        subas(nums,subs,n,ans);
+        int n=nums.size();
+        int b=(1<<n);
+        for(int i=0;i<b;i++){
+            int j=0;
+            vector<int>subs;
+            int k=i;
+            while(k>0){
+                if((k & 1)==1){
+                 subs.push_back(nums[j]);
+                }
+                j++;
+                k=k>>1;
+            } 
+            ans.push_back(subs);
+        }
         return ans;
     }
 };
