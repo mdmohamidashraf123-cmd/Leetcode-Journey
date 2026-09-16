5e136ae9bda6ecd087c613e18d18a8bc6cfff265
MD Mohamid Ashraf <mdmohamidashraf123@gmail.com>
2026-09-16 15:59:08 +0530

    Time: 0 ms (100%), Space: 13.3 MB (66.4%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        //not optimized
+        //Optimized approach
+        //T.C (N)
+        //Concept of buckets
         int n=nums.size();
-        int ans=0;
-        for(int i=0;i<32;i++){
-            int c=0;
-            for(int j=0;j<n;j++){
-                if(nums[j] & (1<<i)){
-                 c++;
-                }
-            }
-            if(c%3!=0){
-              ans=ans|(1<<i);
-            }
+        int ones=0;
+        int twos=0;
+        for(int i=0;i<n;i++){
+            ones=(ones^nums[i])& ~(twos);
+            twos=(twos^nums[i]) & ~(ones);
         }
-        return ans;
+        return ones;
     }
 };
